295030fe4a6b31fd1cff314a1361a7003f39aa8e
Sharaj00 <lulfyxd@gmail.com>
2025-04-25 03:35:31 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "sharaj source stuff's",
     "author": "sharaj00",
     "version": (1, 3, 0),
-    "blender": (3, 00, 0),
+    "blender": (4, 00, 0),
     "category": "Object",
     "description": "some tools for work with source models",
 }
