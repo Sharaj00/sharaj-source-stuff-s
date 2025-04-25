6a422d750f52f285fadb990f349904b6ce1279f2
Sharaj00 <lulfyxd@gmail.com>
2025-04-25 04:57:47 +0300

    Update __init__.py
    разделение панелей по разным .py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,19 @@
 bl_info = {
     "name": "sharaj source stuff's",
     "author": "sharaj00",
-    "version": (1, 3, 0),
+    "version": (1, 3, 1),
     "blender": (4, 00, 0),
     "category": "Object",
     "description": "some tools for work with source models",
 }
 
-from .menu import register, unregister
+from . import menu
+from . import menu_eyes
+
+def register():
+    menu.register()
+    menu_eyes.register()
+
+def unregister():
+    menu.unregister()
+    menu_eyes.unregister()
