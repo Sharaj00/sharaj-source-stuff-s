7ef58116c775b6172cbe35e0e9965e53573ea01e
Sharaj00 <lulfyxd@gmail.com>
2025-09-01 00:57:00 +0600

    1.6.2

--- operators/generate_vmt.py
+++ operators/generate_vmt.py
@@ -3,9 +3,8 @@ import os
 from bpy.types import Operator
 
 def format_float(f):
-    """Округление до трёх знаков после точки"""
     return f"{f:.3f}".rstrip('0').rstrip('.') if '.' in f"{f:.3f}" else f"{f:.3f}"
-    
+
 def show_generated_vmt_popup(materials):
     def draw(self, context):
         for mat in materials:
@@ -20,7 +19,7 @@ class WM_OT_generate_vmt(Operator):
         wm = context.window_manager
         vmt_path = wm.vmt_path
         blend_dir = bpy.path.abspath(wm.output_path)
-        out_dir = os.path.join(blend_dir, vmt_path)
+        out_dir = os.path.join(blend_dir, "materials", vmt_path)
         os.makedirs(out_dir, exist_ok=True)
 
         generated_materials = []
@@ -29,142 +28,156 @@ class WM_OT_generate_vmt(Operator):
             if not mat.use_nodes:
                 continue
 
-            nt = mat.node_tree
+            # Определяем активный шейдер
             shader_node = None
-            for n in nt.nodes:
+            for n in mat.node_tree.nodes:
                 if n.type in {'BSDF_PRINCIPLED', 'EMISSION', 'BSDF_DIFFUSE', 'BSDF_GLASS'}:
                     shader_node = n
                     break
             if not shader_node:
                 continue
 
-            shader_type = "VertexlitGeneric"
+            vmt_lines = []
             if shader_node.type == 'EMISSION':
-                shader_type = "UnlitGeneric"
-
-            vmt_lines = [f'"{shader_type}"', '{']
-
-            # ---------- Base Color ----------
-            base_color_input = shader_node.inputs.get("Base Color")
-            base_tex = "white"
-            if base_color_input and base_color_input.is_linked:
-                from_node = base_color_input.links[0].from_node
-                if from_node.type == "TEX_IMAGE" and from_node.image:
-                    base_tex = os.path.splitext(os.path.basename(from_node.image.filepath))[0]
-
-            if base_tex != "white":
-                vmt_lines.append(f'\t"$basetexture" "{vmt_path}\\{base_tex}"')
+                vmt_lines.append('"UnlitGeneric"')
             else:
-                vmt_lines.append(f'\t"$basetexture" "white"')
-                if base_color_input:
-                    val = base_color_input.default_value[:3]
-                    vmt_lines.append(f'\t"$color2" "[ {format_float(val[0])} {format_float(val[1])} {format_float(val[2])} ]"')
+                vmt_lines.append('"VertexlitGeneric"')
+            vmt_lines.append('{')
 
-            # ---------- Normal ----------
-            bumpmap = None
-            normal_input = shader_node.inputs.get("Normal")
-            if normal_input and normal_input.is_linked:
-                normal_node = normal_input.links[0].from_node
-                if normal_node.type == "NORMAL_MAP":
-                    color_input = normal_node.inputs.get("Color")
-                    if color_input and color_input.is_linked:
-                        tex_node = color_input.links[0].from_node
-                        if tex_node.type == "TEX_IMAGE" and tex_node.image:
-                            bumpmap = os.path.splitext(os.path.basename(tex_node.image.filepath))[0]
-            if bumpmap:
-                vmt_lines.append(f'\t"$bumpmap" "{vmt_path}\\{bumpmap}"')
-
-            # ---------- Roughness ----------
-            rough_input = shader_node.inputs.get("Roughness")
-            phongtex = None
-            if rough_input and rough_input.is_linked:
-                vmt_lines.append("\n\t// Phong")
-                rough_node = rough_input.links[0].from_node
-                if rough_node.type == "TEX_IMAGE" and rough_node.image:
-                    phongtex = os.path.splitext(os.path.basename(rough_node.image.filepath))[0]
-            if phongtex:
-                vmt_lines.append(f'\t"$phong" "1"')
-                vmt_lines.append(f'\t"$phongexponenttexture" "{vmt_path}\\{phongtex}"')
-                vmt_lines.append(f'\t"$phongboost" "2"')
-                vmt_lines.append(f'\t"$phongfresnelranges" "[0.05 0.5 1.0]"')
-
-            # ---------- Emission ----------
+            # ------------------- PRINCIPLED -------------------
             if shader_node.type == 'BSDF_PRINCIPLED':
-                vmt_lines.append("\n\t// Emission")
+                # Base Color
+                base_color_input = shader_node.inputs.get("Base Color")
+                base_tex = "white"
+                color_val = None
+                if base_color_input:
+                    if base_color_input.is_linked:
+                        from_node = base_color_input.links[0].from_node
+                        if from_node.type == "TEX_IMAGE" and from_node.image:
+                            base_tex = os.path.splitext(os.path.basename(from_node.image.filepath))[0]
+                    else:
+                        color_val = base_color_input.default_value[:3]
+                        
+                if base_tex != "white":
+                    vmt_lines.append(f'\t"$basetexture" "{vmt_path}\\{base_tex}"')
+                else:
+                    vmt_lines.append(f'\t"$basetexture" "white"')
+                    if color_val:
+                        vmt_lines.append(f'\t"$color2" "[ {format_float(color_val[0])} {format_float(color_val[1])} {format_float(color_val[2])} ]"')
+
+                # Normal Map
+                normal_input = shader_node.inputs.get("Normal")
+                if normal_input and normal_input.is_linked:
+                    normal_node = normal_input.links[0].from_node
+                    if normal_node.type == "NORMAL_MAP":
+                        color_input = normal_node.inputs.get("Color")
+                        if color_input and color_input.is_linked:
+                            tex_node = color_input.links[0].from_node
+                            if tex_node.type == "TEX_IMAGE" and tex_node.image:
+                                bumpmap = os.path.splitext(os.path.basename(tex_node.image.filepath))[0]
+                                vmt_lines.append(f'\t"$bumpmap" "{vmt_path}\\{bumpmap}"')
+
+                # Roughness / Phong
+                rough_input = shader_node.inputs.get("Roughness")
+                rough_val = rough_input.default_value if rough_input else None
+                phongtex = None
+                if rough_input and rough_input.is_linked:
+                    rough_node = rough_input.links[0].from_node
+                    if rough_node.type == "TEX_IMAGE" and rough_node.image:
+                        phongtex = os.path.splitext(os.path.basename(rough_node.image.filepath))[0]
+
+                if phongtex or (rough_input and not rough_input.is_linked and rough_val < 0.5):
+                    vmt_lines.append(f'\t"$phong" "1"')
+                    if phongtex:
+                        vmt_lines.append(f'\t"$phongexponenttexture" "{vmt_path}\\{phongtex}"')
+                    else:
+                        vmt_lines.append(f'\t"$phongexponent" "32"')
+                    vmt_lines.append('\t"$phongboost" "2"')
+                    vmt_lines.append('\t"$phongfresnelranges" "[0.05 0.5 1.0]"')
+
+                # Emission
                 em_color_input = shader_node.inputs.get("Emission Color")
                 em_strength_input = shader_node.inputs.get("Emission Strength")
                 em_strength = em_strength_input.default_value if em_strength_input else 0
+                em_tex = "white"
+                if em_color_input and em_color_input.is_linked:
+                    from_node = em_color_input.links[0].from_node
+                    if from_node.type == "TEX_IMAGE" and from_node.image:
+                        em_tex = os.path.splitext(os.path.basename(from_node.image.filepath))[0]
                 if em_strength > 0:
-                    em_tex = "white"
-                    if em_color_input and em_color_input.is_linked:
-                        from_node = em_color_input.links[0].from_node
-                        if from_node.type == "TEX_IMAGE" and from_node.image:
-                            em_tex = os.path.splitext(os.path.basename(from_node.image.filepath))[0]
-
-                    vmt_lines.append('\t"$EmissiveBlendEnabled" "1"')
-                    if em_tex != "white":
-                        vmt_lines.append(f'\t"$EmissiveBlendBaseTexture" "{vmt_path}\\{em_tex}"')
-                    else:
-                        vmt_lines.append('\t"$EmissiveBlendBaseTexture" "white"')
+                    em_texture_path = f'{vmt_path}\\{em_tex}' if em_tex != "white" else "white"
+                    vmt_lines.append(f'\t"$EmissiveBlendEnabled" "1"')
+                    vmt_lines.append(f'\t"$EmissiveBlendBaseTexture" "{em_texture_path}"')
                     vmt_lines.append(f'\t"$EmissiveBlendStrength" "{format_float(em_strength)}"')
                     vmt_lines.append('\t"$EmissiveBlendTexture" "white"')
+
+                # Glass / Metallic
+                trans_weight_input = shader_node.inputs.get("Transmission Weight")
+                transmission_val = trans_weight_input.default_value if trans_weight_input else 0
+                metallic_input = shader_node.inputs.get("Metallic")
+                metallic_val = metallic_input.default_value if metallic_input else 0
+
+                if transmission_val > 0 and metallic_val <= 0:
+                    vmt_lines.append('\t"$additive" "1"')
+                    vmt_lines.append('\t"$envmap" "env_cubemap"')
+                    col_input = shader_node.inputs.get("Base Color")
+                    if col_input and col_input.is_linked:
+                        vmt_lines.append('\t"$envmaptint" "[0.5 0.5 0.5]"')
+                    elif col_input:
+                        val = col_input.default_value[:3]
+                        vmt_lines.append(f'\t"$envmaptint" "[ {format_float(val[0])} {format_float(val[1])} {format_float(val[2])} ]"')
+                elif metallic_val > 0:
+                    vmt_lines.append('\t"$envmap" "env_cubemap"')
+                    col_input = shader_node.inputs.get("Base Color")
+                    if col_input and col_input.is_linked:
+                        vmt_lines.append('\t"$envmaptint" "[0.01 0.01 0.01]"')
+                    elif col_input:
+                        val = col_input.default_value[:3]
+                        vmt_lines.append(f'\t"$envmaptint" "[ {format_float(val[0])} {format_float(val[1])} {format_float(val[2])} ]"')
+                    vmt_lines.append('\t"$normalmapalphaenvmapmask" "1"')
+
+            # ------------------- GLASS BSDF -------------------
+            elif shader_node.type == 'BSDF_GLASS':
+                col_input = shader_node.inputs.get("Color")
+                color_val_glass = (1.0, 1.0, 1.0)
+                if col_input:
+                    color_val_glass = col_input.default_value[:3]
+
+                vmt_lines.append(f'\t"$basetexture" "white"')
+                vmt_lines.append(f'\t"$color2" "[ {format_float(color_val_glass[0])} {format_float(color_val_glass[1])} {format_float(color_val_glass[2])} ]"')
+                vmt_lines.append('\t"$additive" "1"')
+                vmt_lines.append('\t"$envmap" "env_cubemap"')
+                vmt_lines.append(f'\t"$envmaptint" "[ {format_float(color_val_glass[0])} {format_float(color_val_glass[1])} {format_float(color_val_glass[2])} ]"')
+
+            # ------------------- EMISSION NODE -------------------
             elif shader_node.type == 'EMISSION':
                 col_input = shader_node.inputs.get("Color")
                 tex_name = "white"
-                if col_input and col_input.is_linked:
-                    from_node = col_input.links[0].from_node
-                    if from_node.type == "TEX_IMAGE" and from_node.image:
-                        tex_name = os.path.splitext(os.path.basename(from_node.image.filepath))[0]
+                color_val_em = None
+                if col_input:
+                    if col_input.is_linked:
+                        from_node = col_input.links[0].from_node
+                        if from_node.type == "TEX_IMAGE" and from_node.image:
+                            tex_name = os.path.splitext(os.path.basename(from_node.image.filepath))[0]
+                    else:
+                        color_val_em = col_input.default_value[:3]
 
+                # Генерация параметров
                 if tex_name != "white":
                     vmt_lines.append(f'\t"$basetexture" "{vmt_path}\\{tex_name}"')
                 else:
                     vmt_lines.append(f'\t"$basetexture" "white"')
-                    val = col_input.default_value[:3]
-                    vmt_lines.append(f'\t"$color2" "[ {format_float(val[0])} {format_float(val[1])} {format_float(val[2])} ]"')
+                    if color_val_em:
+                        vmt_lines.append(f'\t"$color2" "[ {format_float(color_val_em[0])} {format_float(color_val_em[1])} {format_float(color_val_em[2])} ]"')
 
-            # ---------- Glass ----------
-            if shader_node.type == 'BSDF_PRINCIPLED':
-                trans_weight_input = shader_node.inputs.get("Transmission Weight")
-                metallic_input = shader_node.inputs.get("Metallic")
-                metallic_val = metallic_input.default_value if metallic_input else 0
-                transmission_val = trans_weight_input.default_value if trans_weight_input else 0
-
-                # Стекло только если Transmission Weight > 0 и Metallic == 0
-                if transmission_val > 0.0001 and metallic_val <= 0.0001:
-                    vmt_lines.append("\n\t// Glass")
-                    vmt_lines.append('\t"$additive" "1"')
-                    vmt_lines.append('\t"$envmap" "env_cubemap"')
-                    col_input = shader_node.inputs.get("Base Color")
-                    tex_linked = col_input.is_linked if col_input else False
-                    if tex_linked:
-                        vmt_lines.append('\t"$envmaptint" "[ 0.5 0.5 0.5 ]"')
-                    elif col_input:
-                        val = col_input.default_value[:3]
-                        vmt_lines.append(f'\t"$envmaptint" "[ {format_float(val[0])} {format_float(val[1])} {format_float(val[2])} ]"')
-
-            # ---------- Metallic ----------
-            if metallic_val > 0:
-                vmt_lines.append("\n\t// Metallic")
-                mval = metallic_val / 2
-                vmt_lines.append('\t"$envmap" "env_cubemap"')
-                col_input = shader_node.inputs.get("Base Color")
-                tex_linked = col_input.is_linked if col_input else False
-                if tex_linked:
-                    vmt_lines.append('\t"$envmaptint" "[ 0.01 0.01 0.01 ]"')
-                elif col_input:
-                    val = col_input.default_value[:3]
-                    vmt_lines.append(f'\t"$envmaptint" "[ {format_float(val[0])} {format_float(val[1])} {format_float(val[2])} ]"')
-                vmt_lines.append('\t"$normalmapalphaenvmapmask" "1"')
-                
             vmt_lines.append('}\n')
+
             file_path = os.path.join(out_dir, f"{mat.name}.vmt")
             with open(file_path, "w") as f:
                 f.write("\n".join(vmt_lines))
 
             generated_materials.append(mat.name)
 
-        # Выводим сообщение о сгенерированных материалах
         if generated_materials:
             show_generated_vmt_popup(generated_materials)
 
